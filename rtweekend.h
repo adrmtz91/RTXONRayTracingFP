f50dfc060e4815edd1076f98bd5ac4612f5c7550
Adrian Martinez <amart654@ucr.edu>
2024-06-01 14:32:14 -0700

    Simplified main. Code up to chapter 8 with antialiasing

---
+++ rtweekend.h
@@ -0,0 +1,38 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+// C++ Std Usings
+
+using std::make_shared;
+using std::shared_ptr;
+using std::sqrt;
+
+// Constants
+
+const double inf = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+// Common Headers
+#include "interval.h"
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
+
+#endif
