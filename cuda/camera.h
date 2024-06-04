f242dc0c195278ce2fef6f9cc9099f272fe4d384
Adrian Martinez <amart654@ucr.edu>
2024-06-04 05:15:21 -0700

    Output files moved to images folder. Updated camera.h for CUDA.. README displays output images in results.

--- cuda/camera.h
+++ cuda/camera.h
@@ -3,78 +3,36 @@
 #include "ray.h"
 #include "rtweekend.h"
 #include "hittable.h"
-#include <curand_kernel.h> 
+#include <curand_kernel.h>
 
 class camera {
-    public:
-    double aspect_ratio = 1.0;  // Ratio of image width over height
-    int    image_width  = 100;  // Rendered image width in pixel count
-    int    samples_per_pixel = 10;   // Count of random samples for each pixel
+public:
+    double aspect_ratio = 16.0 / 9.0; // Ratio of image width over height
+    int    image_width  = 400; // Rendered image width in pixel count
+    int    samples_per_pixel = 100; // Count of random samples for each pixel
 
-    __device__ ray get_ray(int i, int j, curandState* rand_state) const {
-        // Construct a camera ray originating from the origin and directed at randomly sampled
-        // point around the pixel location i, j.
-
-        auto offset = sample_square(rand_state);
-        auto pixel_sample = pixel00_loc
-                          + ((i + offset.x()) * pixel_delta_u)
-                          + ((j + offset.y()) * pixel_delta_v);
-
-        auto ray_origin = center;
-        auto ray_direction = pixel_sample - ray_origin;
-
-        return ray(ray_origin, ray_direction);
+    __host__ __device__ camera() {
+        initialize();
     }
 
-    __device__ color ray_color(const ray& r, const hittable& world) const {
-        hit_record rec;
+    __device__ ray get_ray(double u, double v, curandState* rand_state) const {
+        return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+    }
 
-        if (world.hit(r, interval(0, inf), rec)) {
-            return 0.5 * (rec.normal + color(1,1,1));
-        }
+    __host__ __device__ void initialize() {
+        auto viewport_height = 2.0;
+        auto viewport_width = aspect_ratio * viewport_height;
+        auto focal_length = 1.0;
 
-        vec3 unit_direction = unit_vector(r.direction());
-        auto a = 0.5*(unit_direction.y() + 1.0);
-        return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+        origin = point3(0, 0, 0);
+        horizontal = vec3(viewport_width, 0.0, 0.0);
+        vertical = vec3(0.0, viewport_height, 0.0);
+        lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
     }
 
 private:
-    int image_height;
-    double pixel_samples_scale;
-    point3 center;
-    point3 pixel00_loc;
-    vec3 pixel_delta_u;
-    vec3 pixel_delta_v;
-
-    __device__ void initialize() {
-        image_height = int(image_width / aspect_ratio);
-        image_height = (image_height < 1) ? 1 : image_height;
-        
-        pixel_samples_scale = 1.0 / samples_per_pixel;
-
-        center = point3(0, 0, 0);
-
-        // Determine viewport dimensions.
-        auto focal_length = 1.0;
-        auto viewport_height = 2.0;
-        auto viewport_width = viewport_height * (double(image_width)/image_height);
-
-        // Calculate the vectors across the horizontal and down the vertical viewport edges.
-        auto viewport_u = vec3(viewport_width, 0, 0);
-        auto viewport_v = vec3(0, -viewport_height, 0);
-
-        // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-        pixel_delta_u = viewport_u / image_width;
-        pixel_delta_v = viewport_v / image_height;
-
-        // Calculate the location of the upper left pixel.
-        
-        auto viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
-        pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-    }
-
-    __device__ vec3 sample_square(curandState* rand_state) const {
-        // Returns the vector to a random point in the [-.5,-.5]-[+.5,+.5] unit square.
-        return vec3(random_double(rand_state) - 0.5, random_double(rand_state) - 0.5, 0);
-    }
+    point3 origin;
+    point3 lower_left_corner;
+    vec3 horizontal;
+    vec3 vertical;
 };
